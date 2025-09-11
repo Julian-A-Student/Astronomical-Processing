0fdb12b8194623cee31bc99f4c77429d5a435b12
P220041 <P220041@tafe.wa.edu.au>
2025-09-11 14:58:30 +0800

    Save today's work

--- Form1.Designer.cs
+++ Form1.Designer.cs
@@ -28,48 +28,113 @@
         /// </summary>
         private void InitializeComponent()
         {
-            listBox1 = new ListBox();
-            label1 = new Label();
-            button1 = new Button();
+            lbNeutrino = new ListBox();
+            lblNeutrino = new Label();
+            btnSort = new Button();
+            btnSearch = new Button();
+            txtSearch = new TextBox();
+            txtEdit = new TextBox();
+            btnEdit = new Button();
+            txtMessage = new TextBox();
+            lblMessage = new Label();
             SuspendLayout();
             // 
-            // listBox1
+            // lbNeutrino
             // 
-            listBox1.FormattingEnabled = true;
-            listBox1.Location = new Point(12, 29);
-            listBox1.Name = "listBox1";
-            listBox1.Size = new Size(144, 454);
-            listBox1.TabIndex = 0;
+            lbNeutrino.FormattingEnabled = true;
+            lbNeutrino.Location = new Point(12, 29);
+            lbNeutrino.Name = "lbNeutrino";
+            lbNeutrino.Size = new Size(167, 454);
+            lbNeutrino.TabIndex = 0;
+            lbNeutrino.SelectedIndexChanged += lbNeutrino_SelectedIndexChanged;
             // 
-            // label1
+            // lblNeutrino
             // 
-            label1.AutoSize = true;
-            label1.Location = new Point(58, 11);
-            label1.Name = "label1";
-            label1.Size = new Size(38, 15);
-            label1.TabIndex = 1;
-            label1.Text = "label1";
+            lblNeutrino.AutoSize = true;
+            lblNeutrino.Location = new Point(12, 9);
+            lblNeutrino.Name = "lblNeutrino";
+            lblNeutrino.Size = new Size(167, 15);
+            lblNeutrino.TabIndex = 1;
+            lblNeutrino.Text = "Neutrino interactions per hour";
             // 
-            // button1
+            // btnSort
             // 
-            button1.Location = new Point(197, 29);
-            button1.Name = "button1";
-            button1.Size = new Size(75, 23);
-            button1.TabIndex = 2;
-            button1.Text = "Sort";
-            button1.UseVisualStyleBackColor = true;
-            button1.Click += button1_Click;
+            btnSort.Location = new Point(214, 29);
+            btnSort.Name = "btnSort";
+            btnSort.Size = new Size(75, 23);
+            btnSort.TabIndex = 2;
+            btnSort.Text = "Sort";
+            btnSort.UseVisualStyleBackColor = true;
+            btnSort.Click += btnSort_Click;
+            // 
+            // btnSearch
+            // 
+            btnSearch.Location = new Point(214, 198);
+            btnSearch.Name = "btnSearch";
+            btnSearch.Size = new Size(75, 23);
+            btnSearch.TabIndex = 3;
+            btnSearch.Text = "Search";
+            btnSearch.UseVisualStyleBackColor = true;
+            btnSearch.Click += btnSearch_Click;
+            // 
+            // txtSearch
+            // 
+            txtSearch.Location = new Point(214, 169);
+            txtSearch.Name = "txtSearch";
+            txtSearch.Size = new Size(75, 23);
+            txtSearch.TabIndex = 4;
+            // 
+            // txtEdit
+            // 
+            txtEdit.Location = new Point(214, 90);
+            txtEdit.Name = "txtEdit";
+            txtEdit.Size = new Size(75, 23);
+            txtEdit.TabIndex = 5;
+            // 
+            // btnEdit
+            // 
+            btnEdit.Location = new Point(214, 119);
+            btnEdit.Name = "btnEdit";
+            btnEdit.Size = new Size(75, 23);
+            btnEdit.TabIndex = 6;
+            btnEdit.Text = "Edit Value";
+            btnEdit.UseVisualStyleBackColor = true;
+            btnEdit.Click += btnEdit_Click;
+            // 
+            // txtMessage
+            // 
+            txtMessage.Location = new Point(185, 262);
+            txtMessage.Multiline = true;
+            txtMessage.Name = "txtMessage";
+            txtMessage.Size = new Size(122, 55);
+            txtMessage.TabIndex = 7;
+            // 
+            // lblMessage
+            // 
+            lblMessage.AutoSize = true;
+            lblMessage.Font = new Font("Segoe UI", 9F, FontStyle.Underline);
+            lblMessage.Location = new Point(223, 244);
+            lblMessage.Name = "lblMessage";
+            lblMessage.Size = new Size(53, 15);
+            lblMessage.TabIndex = 8;
+            lblMessage.Text = "Message";
             // 
             // Form1
             // 
             AutoScaleDimensions = new SizeF(7F, 15F);
             AutoScaleMode = AutoScaleMode.Font;
-            ClientSize = new Size(800, 492);
-            Controls.Add(button1);
-            Controls.Add(label1);
-            Controls.Add(listBox1);
+            ClientSize = new Size(319, 492);
+            Controls.Add(lblMessage);
+            Controls.Add(txtMessage);
+            Controls.Add(btnEdit);
+            Controls.Add(txtEdit);
+            Controls.Add(txtSearch);
+            Controls.Add(btnSearch);
+            Controls.Add(btnSort);
+            Controls.Add(lblNeutrino);
+            Controls.Add(lbNeutrino);
             Name = "Form1";
-            Text = "Form1";
+            Text = "Astronomical Processing";
             Load += Form1_Load;
             ResumeLayout(false);
             PerformLayout();
@@ -77,8 +142,14 @@
 
         #endregion
 
-        private ListBox listBox1;
-        private Label label1;
-        private Button button1;
+        private ListBox lbNeutrino;
+        private Label lblNeutrino;
+        private Button btnSort;
+        private Button btnSearch;
+        private TextBox txtSearch;
+        private TextBox txtEdit;
+        private Button btnEdit;
+        private TextBox txtMessage;
+        private Label lblMessage;
     }
 }
--- Form1.cs
+++ Form1.cs
@@ -1,18 +1,59 @@
+using System.Collections.Generic;
+
 namespace Astronomical_Processing
 {
     public partial class Form1 : Form
     {
+        // Global variables
+        int[] neutrinoArr = new int[24];
+        bool updated = false;
+
         public Form1()
         {
             InitializeComponent();
         }
 
-        private void button1_Click(object sender, EventArgs e)
+        private void createArray()
+        {
+            Random random = new Random();
+            for (int i = 0; i < neutrinoArr.Length; i++)
+            {
+                neutrinoArr[i] = random.Next(10, 90);
+            }
+            updateLB();
+        }
+        public void updateLB(bool changed = true)
+        {
+            updated = changed;
+            lbNeutrino.DataSource = null; // Reset
+            lbNeutrino.DataSource = neutrinoArr;
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            createArray();
+        }
+
+        private void lbNeutrino_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            int index = lbNeutrino.SelectedIndex;
+            if (index != -1)
+            {
+                txtEdit.Text = neutrinoArr[index].ToString();
+            }
+        }
+
+        private void btnSort_Click(object sender, EventArgs e)
         {
 
         }
 
-        private void Form1_Load(object sender, EventArgs e)
+        private void btnEdit_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void btnSearch_Click(object sender, EventArgs e)
         {
 
         }
